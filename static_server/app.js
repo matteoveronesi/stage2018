b622738312e920166679ad85637f51cc56a5958f
matteoveronesi <ex@mp.le>
2018-05-23 22:40:55 +0200

    Update static_server

--- static_server/app.js
+++ static_server/app.js
@@ -1,13 +1,22 @@
 var express = require('express');
 var app = express();
 
+var issues={
+	'Comprare Banane': 'Todo',
+	'Estendere Banane': 'Todo',
+	'Posizionare Banane': 'Todo',
+	'Vendere Banane': 'Done',
+	'Creare Banane': 'Wip'
+};
+
+app.use(express.static('public'));
 app.set('views', './views');
 app.set('view engine', 'pug');
 
 app.get('/', function (req, res) {
   res.render('index', {
-    title: 'Static Server',
-    add: 'Issues Panel' });
+	issues: issues,
+  });
 });
 
 app.listen(8080);
