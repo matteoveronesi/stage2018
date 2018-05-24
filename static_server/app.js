e77d2fb61c3391f67a31e4fc66ab2cb3275cccd4
matteoveronesi <99mv66@gmail.com>
2018-05-24 18:03:17 +0200

    End of Day 4

--- static_server/app.js
+++ static_server/app.js
@@ -1,22 +1,10 @@
 var express = require('express');
+var mainRoute = require('./routes/main');
 var app = express();
 
-var issues={
-	'Comprare Banane': 'Todo',
-	'Estendere Banane': 'Todo',
-	'Posizionare Banane': 'Todo',
-	'Vendere Banane': 'Done',
-	'Creare Banane': 'Wip'
-};
-
-app.use(express.static('public'));
 app.set('views', './views');
 app.set('view engine', 'pug');
+app.use('/', mainRoute);
+app.use(express.static('public'));
 
-app.get('/', function (req, res) {
-  res.render('index', {
-	issues: issues,
-  });
-});
-
-app.listen(8080);
+app.listen(9000);
---
+++ static_server/routes/index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+
+var issues={
+	'Sviluppare prole': 'Todo',
+	'Finire debug di tizio': 'Todo',
+	'Caricare il progetto Bello': 'Wip',
+	'Fare backup server': 'Done',
+	'Comprare nuovo router': 'Wip'
+};
+
+router.get('/', function(req, res) {
+  res.render('index', { issues: issues });
+});
+
+module.exports = router;
